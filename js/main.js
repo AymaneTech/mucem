755a1b327a60f93a6d564037d44821c7f3863f19
aymane <elmainiaymane03@gmail.com>
2023-10-19 12:42:46 +0200

    pushing aymane branch

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-// hamburger navigation
 const hamburger = document.querySelector(".hamburger-icon");
 const navMenu = document.querySelector(".nav-list");
+const faqItems = document.querySelectorAll(".faq-item");
 
+
+// hamburger navigation
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -10,3 +12,14 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+// faq 
+
+faqItems.forEach((item) => {
+    const question = item.querySelector(".question");
+    const answer = item.querySelector(".answer");
+
+    question.addEventListener("click", () => {
+        answer.classList.toggle("active");
+    });
+});
