f2df5c0a85a3a1bf02710248f018697a8a3cdb5c
aymane <elmainiaymane03@gmail.com>
2023-10-19 03:06:10 +0200

    i finish the responsive of home page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// hamburger navigation
+const hamburger = document.querySelector(".hamburger-icon");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
