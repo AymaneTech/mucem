1bb079a3e4657731ff320418deaa38eb1db483f8
aymane <elmainiaymane03@gmail.com>
2023-10-22 11:25:58 +0200

    i do the regex but just using the alert

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const faqItems = document.querySelectorAll(".faq-item");
 
 
 // hamburger navigation
-hamburger.addEventListener("click", ()=>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
@@ -23,3 +23,32 @@ faqItems.forEach((item) => {
         answer.classList.toggle("active");
     });
 });
+
+
+// form variables
+function check() {
+    const form = document.getElementById("form").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+
+
+    if (firstName === "") {
+        alert("please fill name");
+    } 
+    else if (lastName === ""){
+        alert("please fill last name");
+    }
+    else if (email === ""){
+        alert("please fill email");
+    }
+    else if (phone === ""){
+        alert("please fill phone");
+    }
+    else if (message === ""){
+        alert("please fill message");
+    }
+    
+}
