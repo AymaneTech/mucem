ede380ebe8820b030fb3676b09063ab5721b2eff
aymane <elmainiaymane03@gmail.com>
2023-10-24 01:50:22 +0200

    update the regex and some issues in tablette responsive

--- js/main.js
+++ js/main.js
@@ -23,32 +23,32 @@ faqItems.forEach((item) => {
         answer.classList.toggle("active");
     });
 });
-
-
-// form variables
 function check() {
-    const form = document.getElementById("form").value;
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const message = document.getElementById("message").value;
+    const cin = document.getElementById("cin").value;
 
+    var cin_pattern = /^[A-Z]{1,2}[0-9]+$/;
+    var email_pattern = /^[A-Za-z0-9_.]+@([a-z]+.)+[a-z]{2,3}$/;
+    var phone_pattern = /^\+212[0-9]{9}$/;
+
+    const errorMessages = document.querySelectorAll(".error");
+    errorMessages.forEach((error) => error.classList.remove("active"));
 
     if (firstName === "") {
-        alert("please fill name");
-    } 
-    else if (lastName === ""){
-        alert("please fill last name");
+        document.querySelector("#firstName + .error").classList.add("active");
+    } else if (lastName === "") {
+        document.querySelector("#lastName + .error").classList.add("active");
+    } else if (email === "" || !email_pattern.test(email)) {
+        document.querySelector("#email + .error").classList.add("active");
+    } else if (phone === "" || !phone_pattern.test(phone)) {
+        document.querySelector("#phone + .error").classList.add("active");
+    } else if (cin === "" || !cin_pattern.test(cin)) {
+        document.querySelector("#cin + .error").classList.add("active");
+    } else if (message === "") {
+        document.querySelector("#message + .error").classList.add("active");
     }
-    else if (email === ""){
-        alert("please fill email");
-    }
-    else if (phone === ""){
-        alert("please fill phone");
-    }
-    else if (message === ""){
-        alert("please fill message");
-    }
-    
 }
